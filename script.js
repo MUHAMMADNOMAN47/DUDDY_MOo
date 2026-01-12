2b7f7b0cb9ad316f537cd8a4f17b447ff582ce9a
DUDDY <mn4122216@gmail.com>
2026-01-12 07:54:29 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-Final DUDDY website scripts.
+const blocks=document.querySelectorAll('.block');
+
+const obs=new IntersectionObserver(entries=>{
+  entries.forEach(e=>{
+    if(e.isIntersecting){
+      e.target.style.opacity=1;
+      e.target.style.transform='translateY(0)';
+    }
+  })
+},{threshold:.2});
+
+blocks.forEach(b=>{
+  b.style.opacity=0;
+  b.style.transform='translateY(40px)';
+  b.style.transition='0.9s ease';
+  obs.observe(b);
+});
