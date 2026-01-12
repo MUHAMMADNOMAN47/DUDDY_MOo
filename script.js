be45e8f993df650009e01538cd240d1376592a24
DUDDY <mn4122216@gmail.com>
2026-01-12 08:09:39 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,5 @@
-const blocks=document.querySelectorAll('.block');
-
-const obs=new IntersectionObserver(entries=>{
-  entries.forEach(e=>{
-    if(e.isIntersecting){
-      e.target.style.opacity=1;
-      e.target.style.transform='translateY(0)';
-    }
-  })
-},{threshold:.2});
-
-blocks.forEach(b=>{
-  b.style.opacity=0;
-  b.style.transform='translateY(40px)';
-  b.style.transition='0.9s ease';
-  obs.observe(b);
-});
+function copyCA(){
+  const text=document.getElementById("ca").innerText;
+  navigator.clipboard.writeText(text);
+  alert("Contract address copied");
+}
